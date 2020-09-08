9031219db78d1b846ca5acad607d6b287b4fb312
sivagelli <49039230+sivagelli@users.noreply.github.com>
2020-09-08 12:40:13 +0530

    Update Min_Swap_in_Array.py

--- Min_Swap_in_Array.py
+++ Min_Swap_in_Array.py
@@ -3,6 +3,20 @@
 Created on Tue Sep  8 11:08:29 2020
 
 @author: siva
+
+Minimum Swaps 2
+You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order.
+
+For example, given the array  we perform the following steps:
+
+i   arr                         swap (indices)
+0   [7, 1, 3, 2, 4, 5, 6]   swap (0,3)
+1   [2, 1, 3, 7, 4, 5, 6]   swap (0,1)
+2   [1, 2, 3, 7, 4, 5, 6]   swap (3,4)
+3   [1, 2, 3, 4, 7, 5, 6]   swap (4,5)
+4   [1, 2, 3, 4, 5, 7, 6]   swap (5,6)
+5   [1, 2, 3, 4, 5, 6, 7]
+It took  swaps to sort the array.
 """
 
 
@@ -36,7 +50,7 @@ def minimumSwaps(arr):
             arr[getIndex[i]-1]=arr[i-1]
             
             swaps+=1
-    print(getIndex)
+    #print(getIndex)
     return swaps
 
 if __name__ == '__main__':
